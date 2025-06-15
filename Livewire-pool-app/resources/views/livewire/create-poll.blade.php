3c800c6b8f76fab121b87917c7ca298b330b3286
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-15 14:00:34 -0300

    Removing options

--- Livewire-pool-app/resources/views/livewire/create-poll.blade.php
+++ Livewire-pool-app/resources/views/livewire/create-poll.blade.php
@@ -5,15 +5,20 @@
         Current title: {{ $title }}
 
         <div class="mb-4">
-            <button wire:click.prevent="addOption" class="btn">Add Option</button>
+            <button wire:click.prevent="addOption" class="btn mb-4">Add Option</button>
         <div>
 
 
         <div>
             @foreach ( $options as $index => $option )
                 <div class="mb-4">
-                    {{ $index }} - {{ $option }}
+                    <label>Option {{ $index + 1 }}</label>
+                    <div class = "flex-col mb-2 gap-2">
+                        <input type="text" wire:model= "options.{{$index}}"/>
+                        <button class = "btn mb-4" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    <div>
                 </div>
+
             @endforeach
         </div>
     </form>
